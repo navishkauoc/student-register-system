b7a637c46084cf6e0e78b87c12ae95fd83414803
Navishka Kularathna <navishka.randika@gmail.com>
2020-03-17 15:10:31 +0530

    Controller of AngularJS

---
+++ StudentApp/src/main/webapp/app/general.controller.js
@@ -0,0 +1,36 @@
+angular.module("crudApp").controller("GeneralController",GeneralController);
+
+GeneralController.inject=['$scope','Student'];
+
+function GeneralController($scope,Student){
+	$scope.students = Student.query();
+	$scope.student = {};
+	$scope.buttonText = "Submit";
+	
+	$scope.saveStudent = function(){
+		if($scope.student.id != undefined){
+			Student.update($scope,student,function(){
+				$scope.students = Student.query();
+				$scope.student = {};
+				$scope.buttonText = "Submit";
+			});
+		} else{
+			Student.save($scope,student,function(){
+				$scope.students = Student.query();
+				$scope.student = {};
+			});
+		}
+	}
+	
+	$scope.updateStudentInit = function(student){
+		$scope.buttonText = "Update";
+		$scope.student = student;
+	}
+	
+	$scope.deleteStudent = function(student){
+		student.$delete({id:student.id},function(){
+			$scope.students=Students.query();
+		});
+	}
+	
+}
