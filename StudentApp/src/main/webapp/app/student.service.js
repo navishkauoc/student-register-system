0df4ba06e9c03666f217a9d01b68b51cee57a34e
Navishka Kularathna <navishka.randika@gmail.com>
2020-03-17 14:35:26 +0530

    Creating the factory

---
+++ StudentApp/src/main/webapp/app/student.service.js
@@ -0,0 +1,12 @@
+angular.module('crudApp').factory('Student',Student);
+
+Student.$inject=['$resource'];
+
+function Student($resource){
+	var resourceUrl = 'api/student/:id';
+	return $resource(resourceUrl,{},{
+		'update':{
+			method:'PUT'
+		}
+	});
+}
